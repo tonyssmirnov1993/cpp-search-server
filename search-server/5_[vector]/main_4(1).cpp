08cc38f24af6ae9fc7f3da85243d77383efa367a
tonyssmirnov1993 <TonyS33@yandex.ru>
2023-09-05 21:09:30 +0300

    My last commit

---
+++ search-server/5_[vector]/main_4(1).cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int main() {
+    int q;
+    cin >> q;
+    vector<bool> nervy; // создайте вектор типа vector<bool> для хранения очереди
+    for (int i = 0; i < q; ++i) {
+        string operation_code;
+        cin >> operation_code;
+        if (operation_code == "WORRY"s) {
+            int index;
+            cin >> index;
+            nervy[index] = true; // Отметьте человека по индексу index как беспокоящегося (значение true)           
+        } else if (operation_code == "HAPPY"s) {
+            int index;
+            cin >> index;
+            nervy[index] = true; // Отметьте человека по индексу index как спокойного (значение false).
+        } else if (operation_code == "COME"s) {
+            int count;
+            cin >> count;
+            nervy.resize(nervy.size() + count, false); //Измените размер на count. Указывайте второй параметр метода resize, чтобы добавлялись только спокойные (значение false).
+        } else if (operation_code == "LAST_WORRY"s)  {
+            if (nervy.back()) {
+                cout << "worry"s << endl; // Выведите worry, если последний человек беспокойный
+            } else {
+                cout << "happy"s << endl; // Выведите happy, если последний человек спокойный
+            }
+        }
+    }
+}
