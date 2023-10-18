e33ff4621b4d5a6c8e416b571f7570a5f8f2f0c7
tonyssmirnov1993 <TonyS33@yandex.ru>
2023-10-18 17:33:52 +0300

    Sprint_2

---
+++ search-server/Sprint_2/6_subject[numeric_types]/9_double_colon/main_9(1).cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+class SearchServer {
+public:
+    // перенесите сюда DocumentStatus
+    enum class DocumentStatus {
+    ACTUAL,
+    IRRELEVANT,
+    BANNED,
+    REMOVED,
+};
+};
+
+int main() {
+    // выведите численное значение статуса BANNED
+    cout << static_cast<int>(SearchServer::DocumentStatus::BANNED) << endl;
+    return 0;
+}
