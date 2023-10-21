608c53a2571bcd535e296005bcbd1038031a395b
tonyssmirnov1993 <TonyS33@yandex.ru>
2023-10-21 10:34:31 +0300

    Sprint_2

---
+++ search-server/Sprint_2/8_subject[f-templates]/2_term_frequencies/main_2(1).cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <map>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+map<string, int> ComputeTermFreqs(const vector<string>& terms) {
+    // реализуйте функцию 
+    map<string, int> temp; 
+    for(auto word : terms){
+        ++temp[word];
+        }
+    return temp;
+}
+
+int main() {
+    const vector<string> terms = {"first"s, "time"s, "first"s, "class"s};
+    for (const auto& [term, freq] : ComputeTermFreqs(terms)) {
+        cout << term << " x "s << freq << endl;
+    }
+}
